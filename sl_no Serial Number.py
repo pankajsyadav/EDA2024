ba8108ad38d75085a71a6c14278724037d5b9d6f
pankajsyadav <pankajsyadav@users.noreply.github.com>
2025-03-03 00:46:45 -0600

    final

---
+++ sl_no Serial Number.py
@@ -0,0 +1,15 @@
+sl_no               Serial Number
+gender              Gender [ Male (M) / Female (F)]
+ssc_p               10th Grade Percentage
+ssc_b               10th Grade School Board
+hsc_p               12th Grade Percentage
+hsc_b               12th Grade School Board
+hsc_s               12th Grade specialisation
+degree_p            UnderGrad Percentage
+degree_t            UnderGrad Specialization
+workex              Prior Work Experience 
+etest_p             Employability Test Percentage 
+specialisation      Gradudation ( MBA ) Specialization (Fin or HR)
+mba_p               MBA Percentage
+status              Status if placed in Job or not placed
+salary              Salary Offered if placed
